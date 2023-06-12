2b03d79526289769cdc4ebdf62da4c57296b9da1
SoftwareDev15 <jarunya.15th@gmail.com>
2023-06-12 08:47:33 +0700

    swiper join_us

--- assets/js/main.js
+++ assets/js/main.js
@@ -268,6 +268,20 @@
   });
 
 
+ new Swiper(".aomam-slider", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
     /**
    * product Slider
    */
